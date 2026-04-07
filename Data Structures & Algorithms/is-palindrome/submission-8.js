ac90e38d70c02e6fdb6f19cbdc4aaf32aa14fead
Vandit Kala <69248780+111vandit111@users.noreply.github.com>
2026-04-07 13:05:25 +0530

    Add: is-palindrome - submission-8

---
+++ Data Structures & Algorithms/is-palindrome/submission-8.js
@@ -0,0 +1,33 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @return {boolean}
+     */
+    giveValidSmall(val) {
+        const charCode = val.charCodeAt(0);
+        if(charCode >= 97 && charCode <= 122) return charCode - 32;
+        else if (charCode >= 65 && charCode <= 90) return charCode;
+        else if(charCode >= 48 && charCode <= 57) return charCode;
+        else return null;
+    }
+    isPalindrome(s) {
+        let i = 0, k = s.length-1;
+        
+        while(i<k){
+           const val1 = this.giveValidSmall(s[i]);
+           const val2 = this.giveValidSmall(s[k]);
+
+           if(!val1) {i++; continue;}
+           if(!val2) {  k--; continue;};
+           if(val1 === val2){
+            i++;
+            k--;
+            continue;
+           }
+
+           return false;
+        }
+
+        return true;
+    }
+}
