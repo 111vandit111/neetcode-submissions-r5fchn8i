5fbb16bf4ab870368c8f9fc1110f8c38d3f557f6
Vandit Kala <69248780+111vandit111@users.noreply.github.com>
2026-04-14 00:30:52 +0530

    Add: minimum-window-with-characters - submission-3

---
+++ Data Structures & Algorithms/minimum-window-with-characters/submission-3.js
@@ -0,0 +1,43 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @param {string} t
+     * @return {string}
+     */
+    minWindow(s, t) {
+        const tMap = {}, window = {};
+        if(t.length === 0 || s.length === 0 ) return "";
+        for(let i = 0;i<t.length;i++){
+            tMap[t[i]] = (tMap[t[i]] || 0) + 1;
+        }
+
+        let res = [], resLen = 1111111;
+
+        let left = 0, right = 0, count = 0;
+        let requiredCount = Object.keys(tMap).length;
+
+        while(right < s.length){
+            let c = s[right];
+            window[c] = (window[c] || 0) + 1;
+            if(tMap[c] && tMap[c] === window[c]){
+               count++;
+            }
+            while(count === requiredCount){
+                if((right - left + 1) < resLen){
+                    res = [left,right];
+                    resLen = right - left + 1;
+                }
+                window[s[left]] -= 1;
+                if(tMap[s[left]] && tMap[s[left]] > window[s[left]]){
+                    count -= 1;
+                }
+                left += 1;
+            }
+            right++;
+        }
+
+
+        if (resLen === 1111111) return "";
+        return s.slice(res[0],res[1]+1);
+    }
+}
