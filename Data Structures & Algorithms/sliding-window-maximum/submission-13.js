317274fbd83d4fb248a69663b9a1e10aeb4d39a2
Vandit Kala <69248780+111vandit111@users.noreply.github.com>
2026-04-15 01:17:45 +0530

    Add: sliding-window-maximum - submission-13

---
+++ Data Structures & Algorithms/sliding-window-maximum/submission-13.js
@@ -0,0 +1,44 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @param {number} k
+     * @return {number[]}
+     */
+    getCurrentMax(nums, start,end){
+      let currMax = nums[start];
+      start++;
+      console.log({start,end})
+      while(start <= end){
+         const max = Math.max(nums[start], nums[end]);
+         currMax = Math.max(currMax,max);
+         start++; end--;
+      }
+      return currMax;
+    }
+
+    maxSlidingWindow(nums, k) {
+        const res = [];
+        let start = 0;
+        let maxWindow = Math.min(k,nums.length)-1;
+        let currMax = this.getCurrentMax(nums,0,maxWindow);
+
+        while(maxWindow < nums.length){
+            res.push(currMax);
+            console.log({maxWindow,nusm : nums.length})
+            start++;
+            maxWindow++;
+            const newAdd = nums[maxWindow];
+            if(newAdd > currMax){
+                currMax = newAdd;
+            }else{
+                if(currMax > newAdd){
+                    if(currMax === nums[start-1]){
+                        currMax = this.getCurrentMax(nums,start,maxWindow);
+                    }
+                }
+            }
+        }
+
+        return res;
+    }
+}
