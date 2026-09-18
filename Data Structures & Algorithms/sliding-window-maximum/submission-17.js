69eeb97858a22ef31bfd2e6350488fc319f94bcb
Vandit Kala <69248780+111vandit111@users.noreply.github.com>
2026-09-19 00:49:47 +0530

    Add: sliding-window-maximum - submission-17

---
+++ Data Structures & Algorithms/sliding-window-maximum/submission-17.js
@@ -0,0 +1,27 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @param {number} k
+     * @return {number[]}
+     */
+
+    maxSlidingWindow(nums, k) {
+        const res = [];
+        let deque = [];
+        
+        for(let r =0 ;r<nums.length;r++){
+            while(deque.length && nums[deque[deque.length-1]]<nums[r]) deque.pop();
+
+            deque.push(r);
+
+            let l = r-k+1;
+
+            if(deque[0] < l) deque.shift();
+
+            if(l>=0) res.push(nums[deque[0]]);
+        }
+
+        return res;
+
+    }
+}
